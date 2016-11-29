06511029ed4afb05984917d290143783209b8e05
StrangeTim <strang3tim@gmail.com>
2016-11-28 22:22:19 -0800

    third exercise

---
+++ app/vanilla-sum.js
@@ -0,0 +1,6 @@
+var numb1;
+numb1 = parseInt(prompt('please enter a number'));
+var numb2;
+numb2 = parseInt(prompt('please enter another number'));
+var sum = numb1 + numb2;
+alert("The sum of your two numbers is: " + sum);
